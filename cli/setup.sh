96ff02d071bc537104a8689b086f8aa7c2887f34
Scott Yiu <yuyeungscottyiu@gmail.com>
2023-04-23 22:16:35 +0100

    hmmm2...

--- cli/setup.sh
+++ cli/setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="DS_SPRINT_2022"
+LOCATION="uksouth"
+WORKSPACE="ML_MLOps"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
